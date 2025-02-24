e4892e79d4688371815613117813b1689b1da3a6
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-24 17:32:59 +0900

    design: Home.jsx 구성

--- mbtitest/src/components/SectionCard.jsx
+++ mbtitest/src/components/SectionCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const SectionCard = ({ title, description }) => (
   <div className="bg-white p-6 shadow-md rounded-lg text-center">
     <h2 className="text-xl font-semibold text-red-600">{title}</h2>
-    <p className="text-gray-600 mt-2">{description}</p>
+    <p className="text-gray-600 mt-2 text-left">{description}</p>
   </div>
 );
 

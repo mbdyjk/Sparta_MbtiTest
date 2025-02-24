53ed1e2f39fde6ce7f27b0de792f61dc0f353193
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-24 16:35:56 +0900

    feat: 기본 Header 구성 완료

--- mbtitest/src/components/Header.jsx
+++ mbtitest/src/components/Header.jsx
@@ -1,7 +1,26 @@
+import NavLink from "./common/NavLink";
+import Button from "./common/Button";
+
 const Header = () => {
+  const isLogin = false;
+
   return (
     <header className="flex justify-between items-center w-full p-4 bg-white border-b border-gray-200 shadow-sm text-red-600">
-      <div className="space-x-4"></div>
+      <NavLink to="/" className="text-xl font-semibold">
+        홈
+      </NavLink>
+      <div className="space-x-4">
+        {!isLogin ? (
+          <NavLink to="/login">로그인</NavLink>
+        ) : (
+          <>
+            <NavLink to="/profile">프로필</NavLink>
+            <NavLink to="/test">테스트</NavLink>
+            <NavLink to="/results">결과보기</NavLink>
+            <Button className="bg-red-800 hover:bg-red-700">로그아웃</Button>
+          </>
+        )}
+      </div>
     </header>
   );
 };

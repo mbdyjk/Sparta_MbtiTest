9e1c4eef90d2264dd6fc8dad17f0b17ec1c1c8b1
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-24 16:37:41 +0900

    design: 로그아웃 버튼 스타일 수정

--- mbtitest/src/components/Header.jsx
+++ mbtitest/src/components/Header.jsx
@@ -2,7 +2,7 @@ import NavLink from "./common/NavLink";
 import Button from "./common/Button";
 
 const Header = () => {
-  const isLogin = false;
+  const isLogin = true;
 
   return (
     <header className="flex justify-between items-center w-full p-4 bg-white border-b border-gray-200 shadow-sm text-red-600">
@@ -17,7 +17,9 @@ const Header = () => {
             <NavLink to="/profile">프로필</NavLink>
             <NavLink to="/test">테스트</NavLink>
             <NavLink to="/results">결과보기</NavLink>
-            <Button className="bg-red-800 hover:bg-red-700">로그아웃</Button>
+            <Button className="bg-red-700 hover:bg-red-600 text-white">
+              로그아웃
+            </Button>
           </>
         )}
       </div>

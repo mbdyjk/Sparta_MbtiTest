f6d62eaf0e050ebd5ba69a29472c83add3dfdc4b
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-24 15:38:37 +0900

    feat: 공통 컴포넌트 Header 추가

---
+++ mbtitest/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center w-full p-4 bg-white border-b border-gray-200 shadow-sm text-red-600">
+      <div className="space-x-4"></div>
+    </header>
+  );
+};
+
+export default Header;

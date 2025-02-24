14fd4af20377e149e1ceaee78b1658fe40e5f57d
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-25 08:56:57 +0900

    design: AuthForm 및 Login 초기 화면 구성

--- mbtitest/src/routes/ProtectedRoute.jsx
+++ mbtitest/src/routes/ProtectedRoute.jsx
@@ -6,7 +6,6 @@ const ProtectedRoute = () => {
   const { isLogin } = useAuth();
 
   if (!isLogin) {
-    console.log("로그인 필요");
     toast.warn("로그인이 필요합니다. 로그인 페이지로 이동합니다.");
     return <Navigate to="/login" replace />;
   }

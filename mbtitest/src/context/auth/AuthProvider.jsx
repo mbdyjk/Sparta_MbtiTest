47c5e40ce39a451c0ddfcff3aa18811b4a6ab2bc
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-25 15:55:52 +0900

    feat: 새로고침해도 로그인 상태가 유지되도록 token 저장 방식 사용

--- mbtitest/src/context/auth/AuthProvider.jsx
+++ mbtitest/src/context/auth/AuthProvider.jsx
@@ -1,30 +1,41 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { AuthContext } from "./AuthContext";
+import { getUserProfile } from "../../api/auth";
 
 export const AuthProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const loginStatus = localStorage.getItem("isLogin") === "true";
-    setIsLogin(loginStatus);
-    const user = JSON.parse(localStorage.getItem("currentUser"));
-    setCurrentUser(user);
+    const token = localStorage.getItem("token");
+    console.log("🔍 토큰 확인:", token);
+    if (token) {
+      getUserProfile(token)
+        .then((user) => {
+          setIsLogin(true);
+          setCurrentUser(user);
+        })
+        .catch(() => {
+          logout();
+        });
+    }
   }, []);
 
-  const login = (user) => {
+  const login = (user, token) => {
     setIsLogin(true);
-    localStorage.setItem("isLogin", "true");
+    //localStorage.setItem("isLogin", "true");
     setCurrentUser(user);
-    localStorage.setItem("currentUser", JSON.stringify(user));
+    //localStorage.setItem("currentUser", JSON.stringify(user));
+    localStorage.setItem("token", token);
   };
 
   const logout = () => {
     setIsLogin(false);
-    localStorage.removeItem("isLogin");
+    //localStorage.removeItem("isLogin");
     setCurrentUser(null);
-    localStorage.removeItem("currentUser");
+    //localStorage.removeItem("currentUser");
+    localStorage.removeItem("token");
   };
 
   return (

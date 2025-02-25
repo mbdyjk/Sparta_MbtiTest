f8734ec46fdde1b5c91a715eb9cbf8d7608ff756
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-25 09:06:04 +0900

    design: Signup 초기 화면 구성

--- mbtitest/src/pages/Signup.jsx
+++ mbtitest/src/pages/Signup.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/common/NavLink";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const handleSignup = async () => {};
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 shadow-md rounded-lg w-100">
+        <h1 className="text-3xl font-bold mb-4">회원가입</h1>
+        <AuthForm mode="register" onSubmit={handleSignup} />
+        <div className="mt-4 text-center">
+          <p>
+            이미 계정이 있으신가요?{" "}
+            <NavLink to="/login" className="text-red-500">
+              로그인
+            </NavLink>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
